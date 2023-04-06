3fed07878a512aa188dc9d8c71920483aa623dc8
mariabeznea <mariabeznea@gmail.com>
2023-04-06 13:11:49 +0100

    Imported api data into separate files

---
+++ storage/framework/views/3b83ad5eb446fc7993c89ef449bfc003.php
@@ -0,0 +1,23 @@
+<?php $attributes ??= new \Illuminate\View\ComponentAttributeBag; ?>
+<?php foreach($attributes->onlyProps([
+    'message'
+]) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $attributes = $attributes->exceptProps([
+    'message'
+]); ?>
+<?php foreach (array_filter(([
+    'message'
+]), 'is_string', ARRAY_FILTER_USE_KEY) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $__defined_vars = get_defined_vars(); ?>
+<?php foreach ($attributes as $__key => $__value) {
+    if (array_key_exists($__key, $__defined_vars)) unset($$__key);
+} ?>
+<?php unset($__defined_vars); ?>
+
+<div class="flex space-x-1">
+    <span class="text-white uppercase font-bold">Warning:</span> <p class="bg-yellow-700 text-black font-bold"> <?php echo e($message); ?></p>
+</div><?php /**PATH /Users/mariacatalinabeznea/wdd/workshop-laravel-zero/resources/views/warning.blade.php ENDPATH**/ ?>
