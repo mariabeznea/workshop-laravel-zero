08126713283e487cfefd83e75a058c4ece6133f7
mariabeznea <mariabeznea@gmail.com>
2023-04-06 13:25:00 +0100

    Rebuilt views

--- storage/framework/views/26c33f1db42ad6414c140760dd8737ce.php
+++
@@ -1,5 +0,0 @@
-<div class="mb-1">
-    <div class="w-full py-2 text-center bg-blue-500">
-        This is the Workshop demo CLI app built with <span class="text-green-900">Laravel Zero</span> and <span class="text-green">🍃<i>Termwind</i></span>
-    </div>
-</div><?php /**PATH /Users/mariacatalinabeznea/wdd/workshop-laravel-zero/resources/views/title.blade.php ENDPATH**/ ?>
